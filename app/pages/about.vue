52313e1ceb048298dc4ecdf96d0a79a33ddcc824
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-05 18:29:25 +0800

    scss tailwind integration

---
+++ app/pages/about.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AppAlert from '~/components/AppAlert.vue'
+</script>
+
+<template>
+    <div>
+        <h1>Welcome to the about</h1>
+        <AppAlert> This is an auto-imported component </AppAlert>
+    </div>
+</template>
