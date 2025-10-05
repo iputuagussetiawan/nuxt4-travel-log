cb16074faad04e51959e4ea7f06293ec06319852
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-05 16:18:31 +0800

    daisy ui install

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,24 @@
         <h1 class="text-3xl font-bold underline text-amber-300">
             Hello world!
         </h1>
+        <button class="btn btn-primary">Button</button>
+        <button class="btn btn-neutral">Neutral</button>
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-accent">Accent</button>
+        <button class="btn btn-info">Info</button>
+        <button class="btn btn-success">Success</button>
+        <button class="btn btn-warning">Warning</button>
+        <button class="btn btn-error">Error</button>
+
+        <details class="dropdown">
+            <summary class="btn m-1">open or close</summary>
+            <ul
+                class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+            >
+                <li><a>Item 1</a></li>
+                <li><a>Item 2</a></li>
+            </ul>
+        </details>
     </div>
 </template>
