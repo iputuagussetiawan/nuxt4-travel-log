6e5a144a002e0448d5d1c7b74cc578c136c3fb6d
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-09 06:38:52 +0800

    update sidebar triger

--- app/components/ui/sidebar/SidebarTrigger.vue
+++ app/components/ui/sidebar/SidebarTrigger.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from "vue"
-import { PanelLeft } from "lucide-vue-next"
-import { cn } from "@/lib/utils"
+import type { HTMLAttributes } from 'vue'
+import { PanelRightClose, PanelRightOpen } from 'lucide-vue-next'
+import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { useSidebar } from "./utils"
+import { useSidebar } from './utils'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../tooltip'
 
 const props = defineProps<{
-  class?: HTMLAttributes["class"]
+    class?: HTMLAttributes['class']
 }>()
 
-const { toggleSidebar } = useSidebar()
+const { open, toggleSidebar } = useSidebar()
+// const isOpen = ref(false)
+// const toggleSidebar = () => (isOpen.value = !isOpen.value)
 </script>
 
 <template>
-  <Button
-    data-sidebar="trigger"
-    data-slot="sidebar-trigger"
-    variant="ghost"
-    size="icon"
-    :class="cn('h-7 w-7', props.class)"
-    @click="toggleSidebar"
-  >
-    <PanelLeft />
-    <span class="sr-only">Toggle Sidebar</span>
-  </Button>
+    <Tooltip>
+        <TooltipTrigger as-child>
+            <Button
+                data-sidebar="trigger"
+                data-slot="sidebar-trigger"
+                variant="ghost"
+                size="icon"
+                :class="cn('h-7 w-7', props.class)"
+                @click="toggleSidebar"
+            >
+                <component :is="open ? PanelRightOpen : PanelRightClose" />
+                <span class="sr-only">Toggle Sidebar</span>
+            </Button>
+        </TooltipTrigger>
+        <TooltipContent side="bottom">
+            <p>{{ open ? 'Close Sidebar' : 'Open Sidebar' }}</p>
+        </TooltipContent>
+    </Tooltip>
 </template>
