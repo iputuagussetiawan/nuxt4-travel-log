8a4ddb5d28e67264395be78e8df0e49350f1ff68
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-11 07:22:52 +0800

    add form

---
+++ app/pages/dashboard/locations/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+definePageMeta({
+    layout: 'dashboard'
+})
+</script>
+
+<template>
+    <div class="px-4 w-full h-[200vh]">
+        <h1>All Location</h1>
+    </div>
+</template>
