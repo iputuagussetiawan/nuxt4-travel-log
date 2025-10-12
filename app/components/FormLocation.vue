3c5319576e34e663a5ad2a99ec4e981458ad3ba5
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2025-10-12 16:44:19 +0800

    update form input and location form

---
+++ app/components/FormLocation.vue
@@ -0,0 +1,174 @@
+<script setup lang="ts">
+//1.modules import
+import { toTypedSchema } from '@vee-validate/zod'
+import { useForm } from 'vee-validate'
+import type { FetchError } from 'ofetch'
+import { Button } from '@/components/ui/button'
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
+import { InsertLocationSchema } from '~/db/schema'
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert'
+import { AlertCircle, ArrowLeft } from 'lucide-vue-next'
+import { id } from 'zod/v4/locales'
+
+//2.modules init
+const { $csrfFetch } = useNuxtApp()
+definePageMeta({
+    layout: 'dashboard'
+})
+
+const router = useRouter()
+const submitError = ref('')
+const loading = ref(false)
+const submitted = ref(false)
+const formSchema = toTypedSchema(InsertLocationSchema)
+const { isFieldDirty, handleSubmit, meta } = useForm({
+    validationSchema: formSchema
+})
+
+const mapStore = useMapStore()
+
+//3.methods
+const onSubmit = handleSubmit(async (values) => {
+    try {
+        submitError.value = ''
+        loading.value = true
+        await $csrfFetch('/api/locations', {
+            method: 'POST',
+            body: values
+        })
+        submitted.value = true
+        navigateTo(`/dashboard/locations`)
+    } catch (e) {
+        const error = e as FetchError
+        console.log(error.data.data)
+        console.error(error.statusMessage)
+        submitError.value =
+            error.data?.statusMessage ||
+            error.statusMessage ||
+            'An unknown error occurred'
+    } finally {
+        loading.value = false
+    }
+})
+
+//4.events
+onBeforeRouteLeave(() => {
+    if (!submitted.value && meta.value.dirty) {
+        const confirm = window.confirm('Are you sure you want to leave?')
+        if (!confirm) {
+            return false
+        }
+    }
+    mapStore.addedPoint = null
+    return true
+})
+</script>
+<template>
+    <div>
+        <Alert v-if="submitError" variant="destructive" class="mb-4">
+            <AlertCircle class="w-4 h-4" />
+            <AlertTitle>Error</AlertTitle>
+            <AlertDescription>
+                {{ submitError }}
+            </AlertDescription>
+        </Alert>
+        <form class="space-y-6" @submit.prevent="onSubmit">
+            <fieldset :disabled="loading" class="space-y-6">
+                <FormField
+                    v-slot="{ componentField }"
+                    name="name"
+                    :validate-on-blur="!isFieldDirty"
+                    :disabled="loading"
+                >
+                    <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="text"
+                                placeholder="Your Location Name"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
+                <FormField
+                    v-slot="{ componentField }"
+                    name="description"
+                    :validate-on-blur="!isFieldDirty"
+                >
+                    <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                            <Textarea
+                                placeholder="Type your description here."
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
+
+                <FormField
+                    v-slot="{ componentField }"
+                    name="lat"
+                    :validate-on-blur="!isFieldDirty"
+                >
+                    <FormItem>
+                        <FormLabel>Latitude</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="text"
+                                placeholder="Latitude"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
+                <FormField
+                    v-slot="{ componentField }"
+                    name="long"
+                    :validate-on-blur="!isFieldDirty"
+                >
+                    <FormItem>
+                        <FormLabel>Longitude</FormLabel>
+                        <FormControl>
+                            <Input
+                                type="text"
+                                placeholder="Longitude"
+                                v-bind="componentField"
+                            />
+                        </FormControl>
+                        <FormMessage />
+                    </FormItem>
+                </FormField>
+
+                <div class="flex justify-between">
+                    <Button
+                        :disabled="loading"
+                        type="button"
+                        variant="outline"
+                        @click="router.back()"
+                    >
+                        Cancel
+                    </Button>
+                    <Button :disabled="loading" type="submit">
+                        Submit
+                        <LucideLoader2 v-if="loading" class="animate-spin" />
+                        <LucideCirclePlus v-else />
+                    </Button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+</template>
+<style lang="scss" scoped></style>
--- app/pages/dashboard/locations/index.vue
+++ app/pages/dashboard/locations/index.vue
@@ -1,17 +1,10 @@
 <script setup lang="ts">
 //1.modules import
 import { Button, buttonVariants } from '@/components/ui/button'
-import {
-    ArrowUpRightIcon,
-    FolderCode,
-    LucideCirclePlus,
-    MapPin
-} from 'lucide-vue-next'
+import { ArrowUpRightIcon, LucideCirclePlus, MapPin } from 'lucide-vue-next'
 import {
     Card,
-    CardContent,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle
 } from '~/components/ui/card'
@@ -25,13 +18,12 @@ import {
     EmptyTitle
 } from '@/components/ui/empty'
 
-import MapClient from '~/components/MapClient.vue'
-
 import { cn } from '~/lib/utils'
 import { Skeleton } from '~/components/ui/skeleton'
 import MapClient2 from '~/components/MapClient2.vue'
 const locationsStore = useLocationsStore()
 const { locations, status } = storeToRefs(locationsStore)
+const showForm = ref(false)
 
 const mapStore = useMapStore()
 
@@ -43,120 +35,142 @@ definePageMeta({
 onMounted(() => {
     locationsStore.refresh()
 })
+
+const toggleForm = () => {
+    showForm.value = !showForm.value
+}
 </script>
 
 <template>
-    <section>
-        <div class="mt-4 px-4">
-            <div
-                v-if="locations && locations.length > 0"
-                class="flex justify-between items-center"
-            >
+    <section class="mt-4 px-4">
+        <div class="flex flex-col gap-4">
+            <!-- Header -->
+            <div class="flex justify-between items-center">
                 <div>
-                    <h1 class="text-lg font-bold">List Location</h1>
+                    <h2 class="text-lg font-bold">My Location</h2>
                     <p class="text-sm text-muted-foreground">
-                        List of all locations here
+                        Manage your locations and view them on the map
                     </p>
                 </div>
                 <div class="flex gap-2">
-                    <NuxtLink
-                        to="/dashboard/locations/add"
-                        :class="
-                            cn(buttonVariants({ variant: 'default' }), 'gap-2')
-                        "
-                    >
-                        Add Location
-                        <LucideCirclePlus class="w-4 h-4" />
-                    </NuxtLink>
+                    <Button @click="toggleForm">
+                        {{ showForm ? 'Close Form' : 'Add Location' }}
+                        <LucideCirclePlus class="w-4 h-4 ml-2" />
+                    </Button>
                 </div>
             </div>
-            <div
-                v-if="status === 'pending'"
-                class="grid grid-cols-6 gap-4 mt-6"
-            >
-                <Card v-for="i in 12" :key="i">
-                    <CardHeader>
-                        <CardTitle>
-                            <Skeleton class="h-2 w-1/2" />
-                        </CardTitle>
-                        <CardDescription class="space-y-1">
-                            <Skeleton class="h-2 w-full" />
-                            <Skeleton class="h-2 w-1/2" />
-                        </CardDescription>
-                    </CardHeader>
-                </Card>
-            </div>
-            <div
-                v-else-if="locations && locations.length > 0"
-                class="grid grid-cols-6 gap-4 mt-6"
-            >
-                <Card
-                    v-for="location in locations"
-                    :key="location.id"
-                    @click="mapStore.selectedPoint = location"
-                    @mouseleave="mapStore.selectedPoint = null"
-                    class="bg-primary/10 hover:bg-primary/20 cursor-pointer transition-all duration-400 ease-in-out"
-                    :class="{
-                        'border-primary':
-                            location.id === mapStore.selectedPoint?.id
-                    }"
-                >
-                    <CardHeader>
-                        <CardTitle>{{ location.name }}</CardTitle>
-                        <CardDescription>
-                            {{ location.description }}
-                        </CardDescription>
-                    </CardHeader>
-                    <!-- <CardContent> sdfsdff </CardContent>
-                    <CardFooter> sdfsd </CardFooter> -->
-                </Card>
-            </div>
-            <div v-else class="w-full">
-                <Empty>
-                    <EmptyHeader>
-                        <EmptyMedia variant="icon">
-                            <MapPin class="w-10 h-10 text-muted-foreground" />
-                        </EmptyMedia>
+            <!-- Locations List -->
+            <div v-if="!+showForm">
+                <div v-if="locations && locations.length > 0" class="mt-6">
+                    <h3 class="text-lg font-semibold mb-3">Location List</h3>
 
-                        <EmptyTitle>No Locations Found</EmptyTitle>
-                        <EmptyDescription>
-                            You haven't added any locations yet. Start by
-                            creating your first one to begin tracking and
-                            managing your data.
-                        </EmptyDescription>
-                    </EmptyHeader>
-
-                    <EmptyContent>
-                        <div class="flex gap-2">
-                            <Button asChild>
-                                <NuxtLink to="/dashboard/locations/add">
-                                    Create Location
-                                </NuxtLink>
-                            </Button>
-                        </div>
-                    </EmptyContent>
-
-                    <Button
-                        variant="link"
-                        asChild
-                        class="text-muted-foreground"
-                        size="sm"
+                    <div
+                        v-if="status === 'pending'"
+                        class="grid grid-cols-6 gap-4 mt-6"
                     >
-                        <NuxtLink to="/dashboard">
-                            Learn more about managing locations
-                            <ArrowUpRightIcon class="w-4 h-4 ml-1" />
-                        </NuxtLink>
-                    </Button>
-                </Empty>
-            </div>
-            <div class="mt-6">
-                <!-- <ClientOnly fallback-tag="span" fallback="Loading Map...">
-                    <MapClient />
-                </ClientOnly> -->
+                        <Card v-for="i in 12" :key="i">
+                            <CardHeader>
+                                <CardTitle>
+                                    <Skeleton class="h-2 w-1/2" />
+                                </CardTitle>
+                                <CardDescription class="space-y-1">
+                                    <Skeleton class="h-2 w-full" />
+                                    <Skeleton class="h-2 w-1/2" />
+                                </CardDescription>
+                            </CardHeader>
+                        </Card>
+                    </div>
 
-                <ClientOnly fallback-tag="span" fallback="Loading Map...">
-                    <MapClient2 />
-                </ClientOnly>
+                    <div
+                        v-else
+                        class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4 mt-6"
+                    >
+                        <Card
+                            v-for="location in locations"
+                            :key="location.id"
+                            @click="mapStore.selectedPoint = location"
+                            @mouseleave="mapStore.selectedPoint = null"
+                            class="bg-primary/10 hover:bg-primary/20 cursor-pointer transition-all duration-400 ease-in-out"
+                            :class="{
+                                'border-primary':
+                                    location.id === mapStore.selectedPoint?.id
+                            }"
+                        >
+                            <CardHeader>
+                                <CardTitle>{{ location.name }}</CardTitle>
+                                <CardDescription>
+                                    {{ location.description }}
+                                </CardDescription>
+                            </CardHeader>
+                        </Card>
+                    </div>
+                </div>
+
+                <!-- Empty State -->
+                <div v-else class="w-full mt-6">
+                    <Empty>
+                        <EmptyHeader>
+                            <EmptyMedia variant="icon">
+                                <MapPin
+                                    class="w-10 h-10 text-muted-foreground"
+                                />
+                            </EmptyMedia>
+
+                            <EmptyTitle>No Locations Found</EmptyTitle>
+                            <EmptyDescription>
+                                You haven't added any locations yet. Start by
+                                creating your first one to begin tracking and
+                                managing your data.
+                            </EmptyDescription>
+                        </EmptyHeader>
+
+                        <EmptyContent>
+                            <div class="flex gap-2">
+                                <Button @click="toggleForm">
+                                    Create Location
+                                </Button>
+                            </div>
+                        </EmptyContent>
+
+                        <Button
+                            variant="link"
+                            asChild
+                            class="text-muted-foreground"
+                            size="sm"
+                        >
+                            <NuxtLink to="/dashboard">
+                                Learn more about managing locations
+                                <ArrowUpRightIcon class="w-4 h-4 ml-1" />
+                            </NuxtLink>
+                        </Button>
+                    </Empty>
+                </div>
+            </div>
+
+            <!-- Main Content Area -->
+            <div
+                class="grid grid-cols-1 md:grid-cols-3 gap-4 transition-all duration-300"
+            >
+                <!-- Left Side: Form -->
+                <div v-if="showForm" class="bg-card rounded-xl p-4 shadow-sm">
+                    <h2 class="text-lg font-semibold mb-2">
+                        Create a New Location
+                    </h2>
+                    <p class="text-sm text-muted-foreground mb-4">
+                        Fill in the form to add a new location.
+                    </p>
+                    <FormLocation />
+                </div>
+
+                <!-- Right Side: Map -->
+                <div
+                    class="bg-card rounded-xl p-2 relative overflow-hidden"
+                    :class="showForm ? 'col-span-2 h-[600px]' : 'col-span-3'"
+                >
+                    <ClientOnly fallback-tag="span" fallback="Loading Map...">
+                        <MapClient2 />
+                    </ClientOnly>
+                </div>
             </div>
         </div>
     </section>
